545ae8744b3faf262eeee31e621d40b123c61c8b
Jumatemory <*Case#255>
2020-03-01 16:21:39 -0500

    Fourth message

--- src/project/ClassIntro.java
+++ src/project/ClassIntro.java
@@ -2,10 +2,14 @@ package project;
 
 public class ClassIntro {
     public static void main(String[] args) {
+        display();
         System.out.println("hello world ");
         System.out.println("hello world ");
         System.out.println("hello world ");
         System.out.println("hello world ");
         System.out.println("hello world ");
+    }
+    public static void display (){
+        System.out.println("fist project ");
     }
 }
