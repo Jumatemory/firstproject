69c54abe44d7f30f0df141c8f183297540e46d6e
Jumatemory <*Case#255>
2020-03-01 16:13:32 -0500

    second commit

--- src/project/ClassIntro.java
+++ src/project/ClassIntro.java
@@ -1,4 +1,11 @@
 package project;
 
 public class ClassIntro {
+    public static void main(String[] args) {
+        System.out.println("hello world ");
+        System.out.println("hello world ");
+        System.out.println("hello world ");
+        System.out.println("hello world ");
+        System.out.println("hello world ");
+    }
 }
